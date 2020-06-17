dfec38867f6b0d0298c6b504aa3608dbf3b3e369
Hoang Vu <hoangvu@uafs.edu>
2020-06-16 19:08:03 -0500

    first half of todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: "",
+    todos: [],
+  };
+
+  inputHandle = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  addHandle = () => {
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, this.state.text],
+    }));
+
+    console.log(this.state.todos);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={(e) => this.inputHandle(e)} />
+        <button onClick={this.addHandle}>Add</button>
+        {this.state.todos.map((todo, index) => {
+          return (
+            <div key={index}>
+              {todo}
+              <button>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
